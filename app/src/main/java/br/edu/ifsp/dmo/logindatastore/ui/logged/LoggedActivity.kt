144fd1f13fbfd97821d4ddc37a5d16cf94b3ce04
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-01-12 09:05:55 -0300

    Implementação da activity de logado e de seu view model

--- app/src/main/java/br/edu/ifsp/dmo/logindatastore/ui/logged/LoggedActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/logindatastore/ui/logged/LoggedActivity.kt
@@ -2,11 +2,20 @@ package br.edu.ifsp.dmo.logindatastore.ui.logged
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import br.edu.ifsp.dmo.logindatastore.R
+import br.edu.ifsp.dmo.logindatastore.databinding.ActivityLoggedBinding
 
 class LoggedActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoggedBinding
+    private lateinit var viewModel: LoggedViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_logged)
+        binding = ActivityLoggedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(LoggedViewModel::class.java)
+        binding.textMessage.text = "Bem-vindo!"
     }
 }
