d132fd1b53d87d5b9baa87f9a7b0ecde6883ce48
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-01-12 09:24:23 -0300

    Adição da lógica do main activity

--- app/src/main/java/br/edu/ifsp/dmo/logindatastore/ui/main/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/logindatastore/ui/main/MainActivity.kt
@@ -1,12 +1,86 @@
 package br.edu.ifsp.dmo.logindatastore.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import br.edu.ifsp.dmo.logindatastore.R
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import br.edu.ifsp.dmo.logindatastore.databinding.ActivityMainBinding
+import br.edu.ifsp.dmo.logindatastore.ui.logged.LoggedActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+    private var flag = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        setupObservers()
+        setupListeners()
+    }
+
+    private fun setupObservers() {
+        viewModel.loggedIn.observe(this, Observer {
+            if (it) {
+                navigateToLoggedActivity()
+            } else {
+                Toast.makeText(this, "Erro ao fazer login.", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        viewModel.loginPreferences.observe(this, Observer {
+            val (saveLogin, stayLoggedIn) = it
+            if (stayLoggedIn) {
+                navigateToLoggedActivity()
+            }
+            binding.checkboxSaveLogin.isChecked = saveLogin
+            binding.checkboxStayLoggedin.isChecked = stayLoggedIn
+        })
+
+        viewModel.dataPreferences.observe(this, Observer {
+            val (email, password) = it
+            binding.textEmail.setText(email)
+            if (email.isNotEmpty()) {
+                binding.textPassword.setText(password.toString())
+            }
+        })
+    }
+
+    private fun setupListeners() {
+        binding.buttonLogin.setOnClickListener {
+            handleLogin()
+        }
+    }
+
+    private fun handleLogin() {
+        val email = binding.textEmail.text.toString()
+        val password = binding.textPassword.toString().toLong()
+        val saveLogin = binding.checkboxSaveLogin.isChecked
+        val stayLoggedIn = binding.checkboxStayLoggedin.isChecked
+
+        binding.textEmail.setText("")
+        binding.textPassword.setText("")
+        binding.checkboxSaveLogin.isChecked = false
+        binding.checkboxStayLoggedin.isChecked = false
+
+        if (email.isNotEmpty()) {
+            viewModel.login(email, password, saveLogin, stayLoggedIn)
+        } else {
+            Toast.makeText(this, "Preencha todos os dados.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun navigateToLoggedActivity() {
+        if (!flag) {
+            flag = true
+            val mIntent = Intent(this, LoggedActivity::class.java)
+            startActivity((mIntent))
+            finish()
+        }
     }
 }
