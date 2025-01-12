90bc966b1c029971ce5ca531449707fef90e9659
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-01-12 08:25:55 -0300

    Criação dos diretórios MVVM

---
+++ app/src/main/java/br/edu/ifsp/dmo/logindatastore/ui/logged/LoggedActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.dmo.logindatastore.ui.logged
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo.logindatastore.R
+
+class LoggedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logged)
+    }
+}
--- app/src/main/java/br/edu/ifsp/dmo/logindatastore/ui/main/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/logindatastore/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.edu.ifsp.dmo.logindatastore
+package br.edu.ifsp.dmo.logindatastore.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo.logindatastore.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
